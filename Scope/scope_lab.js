9d59a73de44397d1a14b651addfbc3d626464309
“Anna <annacib@bluewin.ch>
2026-07-09 09:37:17 -0400

    check new repo

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -19,6 +19,7 @@ console.log(globalConst); // Output: "I'm a global constant"
 //console.log(blockLet);
 //console.log(blockConst);
 
+/*
 function show() {
     var functionVar = "Block-scoped var"
     let functionLet = "Block-scoped let"
@@ -29,3 +30,42 @@ show()
 console.log(functionVar); // Throws ReferenceError
 console.log(functionLet); // Throws ReferenceError
 console.log(functionConst); // Throws ReferenceError
+*/
+
+{
+    let var1 = 32
+    const var2 = 34
+    var var3 = 'check'
+    console.log('var1 value', var1)
+    console.log('var2 value', var2)
+    console.log('var3 value', var3)
+
+    console.log('Assigning in block')
+
+    console.log('assigning var1')
+
+    var1 = 2
+    console.log('var1 value', var1)
+
+    console.log('assigning var2')
+
+    //var2 = 4
+
+    console.log('assigning var3')
+    var3 = 5
+    console.log('var3 value', var3)
+
+}
+console.log('Assigning outside block')
+
+console.log('assigning var1')
+var1 += 2
+console.log('var1 value', var1)
+
+console.log('assigning var2')
+var2 = 44
+console.log('var2 value', var2)
+
+console.log('assigning var3')
+var3 = 55
+console.log('var3 value', var3)
